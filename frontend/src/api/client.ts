9f97752456f6188137ba85682350c5512afc4827
Melbromss <melbromss@gmail.com>
2024-03-04 13:57:24 +0700

    Add booking confirmation detail

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,11 +1,25 @@
 import {
   HotelSearchResponse,
   HotelType,
+  UserType,
 } from "../../../backend/src/shared/types";
 import { RegisterFormData } from "../forms/AuthForm/RegisterForm";
 import { SignInFormData } from "../forms/AuthForm/SignInForm";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+
+export const fetchCurrentUser = async ():Promise<UserType> => {
+  const response = await fetch(`${API_BASE_URL}/api/users/me`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error fetching user");
+  }
+
+  return response.json();
+
+}
 
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
